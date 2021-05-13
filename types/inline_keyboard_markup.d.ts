bcf1c2ff50e5025398a98b54b0cce3aa753e187a
le-ar <andreiklauzer@gmail.com>
2021-05-13 20:03:05 +0300

    fix InlineKeyboardMarkup

--- types/inline_keyboard_markup.d.ts
+++ types/inline_keyboard_markup.d.ts
@@ -1,5 +1,5 @@
 import { InlineKeyboardButton } from "./Inline_keyboard_button";
 
 export interface InlineKeyboardMarkup {
-  inline_keyboard: InlineKeyboardButton[];
+  inline_keyboard: InlineKeyboardButton[][];
 }
